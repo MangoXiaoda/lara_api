5c69165902522d724fe70436e1e6bf98a0c54269
Mango <mangoxiaoda@gmail.com>
2019-08-03 03:28:16 +0000

    测试接口

--- routes/api.php
+++ routes/api.php
@@ -2,29 +2,43 @@
 
 use Illuminate\Http\Request;
 
+//$api = app('Dingo\Api\Routing\Router');
+
+//$api->version('v1', [
+//    'namespace' => 'App\Http\Controllers\Api'
+//], function($api) {
+//
+//    $api->group([
+//        'middleware' => 'api.throttle',
+//        'limit' => config('api.rate_limits.sign.limit'),
+//        'expires' => config('api.rate_limits.sign.expires'),
+//    ], function($api){
+//    // 短信验证码
+//    $api->post('verificationCodes', 'VerificationCodesController@store')
+//        ->name('api.verificationCodes.store');
+//
+//    // 用户注册
+//    $api->post('users', 'UsersController@store')
+//        ->name('api.users.store');
+//    });
+//
+//    // 图片验证码
+//    $api->post('captchas', 'CaptchasController@store')
+//        ->name('api.captchas.store');
+//
+//
+//});
+
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-
-    $api->group([
-        'middleware' => 'api.throttle',
-        'limit' => config('api.rate_limits.sign.limit'),
-        'expires' => config('api.rate_limits.sign.expires'),
-    ], function($api){
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+$api->version('v1', function($api) {
+    $api->get('version', function() {
+        return response('this is version v1');
     });
+});
 
-    // 图片验证码
-    $api->post('captchas', 'CaptchasController@store')
-        ->name('api.captchas.store');
-
-
+$api->version('v2', function($api) {
+    $api->get('version', function() {
+        return response('this is version v2');
+    });
 });
