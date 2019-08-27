b673d88480c843bc4bf0e5757b58519df4696282
vagrant <vagrant@homestead>
2019-08-27 09:47:51 +0000

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -44,6 +44,24 @@ class UsersController extends Controller
 
     }
 
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer);
+    }
+
     /**
      * 获取用户信息
      * @return \Dingo\Api\Http\Response
