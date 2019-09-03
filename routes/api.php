fd1246d3266e3de4c05791ea63e135f35be0a765
vagrant <vagrant@homestead>
2019-09-03 03:52:20 +0000

    notifications index

--- routes/api.php
+++ routes/api.php
@@ -106,6 +106,10 @@ $api->version('v1', [
             $api->delete('topics/{topic}/replies/{reply}','RepliesController@destroy')
                 ->name('api.topics.replies.destroy');
 
+            $api->get('user/notifications', 'NotificationsController@index')
+                ->name('api.user.notifications.index');
+
+
 
         });
     });
