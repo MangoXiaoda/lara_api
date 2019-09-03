34f0e0ebf39b4fd2742fff0f91ee1319ef8f278f
vagrant <vagrant@homestead>
2019-09-03 07:32:38 +0000

    links index

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,10 @@ $api->version('v1', [
         $api->get('users/{user}/replies', 'RepliesController@index')
             ->name('api.users.replies.index');
 
+        // 资源推荐
+        $api->get('links', 'LinksController@index')
+            ->name('api.links.index');
+
         // 需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api){
             // 当前登录用户信息
